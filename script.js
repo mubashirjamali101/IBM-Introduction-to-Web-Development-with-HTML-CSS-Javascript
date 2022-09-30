f2a17db15fc4bc25ed9fae7eae48e088ea9a6a1b
Mubashir Jamali <mubashirjamali101@gmail.com>
2022-09-30 20:12:59 +0500

    Add comments

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function compute() {
+    // get the reference to the corresponding html elements
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
+    // check if principal is 0 or less then give alert and focus to principal
     if (principal <= 0) {
         alert("Principal can not be negative or zero!");
         document.getElementById("principal").focus();
         return 0;
     }
 
-    var interest = principal * years * rate * 100;
+    // calculate the amount of interest user will get after the calculated years
+    var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + Number(years);
 
+    // print the result in the browser
     document.getElementById("result").innerHTML = `
     If you deposit <mark>${principal}</mark> </br>
     at an interest rate of <mark>${rate}%</mark>.</br>
@@ -20,6 +24,7 @@ function compute() {
     `;
 }
 
+// updates the rate when slider's value is changed
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = `${rateval}%`;
