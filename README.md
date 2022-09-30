6725119e048dda07618d1fc011815e1e2b59d61a
Mubashir Jamali <mubashirjamali101@gmail.com>
2022-09-30 20:00:04 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IBM Introduction to Web Development with HTMl, CSS, JavaScript
+
+This is the repo for IBM course mentioned above, the course was offered via Coursera platform. And, provided basic understanding of web development fundamentals.
---
+++ script.js
@@ -0,0 +1,26 @@
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal <= 0) {
+        alert("Principal can not be negative or zero!");
+        document.getElementById("principal").focus();
+        return 0;
+    }
+
+    var interest = principal * years * rate * 100;
+    var year = new Date().getFullYear() + Number(years);
+
+    document.getElementById("result").innerHTML = `
+    If you deposit <mark>${principal}</mark> </br>
+    at an interest rate of <mark>${rate}%</mark>.</br>
+    You will recieve an amount of <mark>${interest}</mark> </br>
+    in the year </mark>${year}</mark>
+    `;
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = `${rateval}%`;
+}
